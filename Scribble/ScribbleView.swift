04485ebc834f8c2c77964f4222666240862a6495
Tomek <tomek@japko.info>
2016-06-24 22:18:18 +0200

    Naive rendering of strokes.

--- Scribble/ScribbleView.swift
+++ Scribble/ScribbleView.swift
@@ -12,6 +12,8 @@ class ScribbleView: UIView {
 
     var strokes:[Stroke] = [ Stroke(points:[CGPoint(x: 10,y: 10), CGPoint(x: 100,y: 100)]) ]
     
+    var currentStroke:[CGPoint] = []
+    
     
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -19,12 +21,13 @@ class ScribbleView: UIView {
         // Drawing code
         let ctx = UIGraphicsGetCurrentContext()
         
+        strokes = [Stroke(points:currentStroke)]
+        
         strokes.forEach { (stroke:Stroke) in
             if (stroke.points.count > 1) {
                 let h = stroke.points.first!
                 CGContextMoveToPoint(ctx, h.x, h.y)
-                let count = stroke.points.count
-                let tail = stroke.points[1..<count]
+                let tail = stroke.points.dropFirst()
                 tail.forEach({ (p:CGPoint) in
                     CGContextAddLineToPoint(ctx, p.x, p.y)
                 })
@@ -32,9 +35,40 @@ class ScribbleView: UIView {
                 CGContextSetLineWidth(ctx, 4.0)
                 CGContextSetLineCap(ctx, CGLineCap.Round)
                 CGContextStrokePath(ctx)
+            } else {
+                if let loc = stroke.points.first {
+                    let frame = CGRect(origin: loc, size: CGSize(width: 4.0, height: 4.0))
+                    CGContextSetFillColorWithColor(ctx, UIColor.blackColor().CGColor)
+                    CGContextFillEllipseInRect(ctx, frame)
+                }
             }
         }
         
     }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let loc = touches.first?.locationInView(self) {
+            print("TOUCH \(loc)")
+            
+            currentStroke = [CGPoint]()
+            currentStroke.append(loc)
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let loc = touches.first?.locationInView(self) {
+            print("LINE \(loc)")
+            currentStroke.append(loc)
+            self.setNeedsDisplay()
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let loc = touches.first?.locationInView(self) {
+            print("END \(loc)")
+        }
+        self.setNeedsDisplay()
+    }
+
 
 }
