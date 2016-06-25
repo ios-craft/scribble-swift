84fec4c829c08fae3156a43f7edcdbf060612a57
Tomek <tomek@japko.info>
2016-06-25 23:31:36 +0200

    Color picker

--- Scribble/PaletteViewController.swift
+++ Scribble/PaletteViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class PaletteViewController: UIViewController {
 
+    let colorArray = [ 0x000000, 0xfe0000, 0xff7900, 0xffb900, 0xffde00, 0xfcff00, 0xd2ff00, 0x05c000, 0x00c0a7, 0x0600ff, 0x6700bf, 0x9500c0, 0xbf0199, 0xffffff ]
+
     @IBAction func sizeSliderChanged(sender: UISlider) {
         let value = sender.value
         sliderSizeLabel.text = "\(round(value)) points"
@@ -17,7 +19,14 @@ class PaletteViewController: UIViewController {
     
     @IBOutlet var sliderSizeLabel: UILabel!
     
+    @IBOutlet var colorSampler: UIView!
     
+    @IBAction func colorValueChanged(sender: UISlider) {
+
+        let colorId = colorArray[Int(sender.value)]
+        let color = uiColorFromHexValue(colorId)
+        colorSampler.backgroundColor = color
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +39,15 @@ class PaletteViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func uiColorFromHexValue(rgbValue: Int) -> UIColor {
+        
+        let red =   CGFloat((rgbValue & 0xFF0000) >> 16) / 0xFF
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 0xFF
+        let blue =  CGFloat(rgbValue & 0x0000FF) / 0xFF
+        let alpha = CGFloat(1.0)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
 
     /*
     // MARK: - Navigation
